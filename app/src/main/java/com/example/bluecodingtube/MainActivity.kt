68b206062233de00bcba20a4b5b17f092e132048
iWantBigMan <skarndgus8899@gmail.com>
2023-09-25 20:14:08 +0900

    [NEW] : 🆕 First Commit

---
+++ app/src/main/java/com/example/bluecodingtube/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.bluecodingtube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
+import com.example.bluecodingtube.databinding.ActivityMainBinding
+import com.example.codingtube.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView(){
+        var viewPagerAdapter = ViewPagerAdapter(this)
+        viewPagerAdapter.addFragment(DetailPage())
+        viewPagerAdapter.addFragment(SearchPage())
+        viewPagerAdapter.addFragment(MyPage())
+
+        binding.mainView.apply {
+            adapter = viewPagerAdapter
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            })
+        }
+//        val iconList = ArrayList<Drawable?>()
+//        iconList.add(ContextCompat.getDrawable(this, R.drawable.baseline_home_24))
+//        iconList.add(ContextCompat.getDrawable(this, R.drawable.baseline_search_24))
+//        iconList.add(ContextCompat.getDrawable(this, R.drawable.baseline_toc_24))
+
+        TabLayoutMediator(binding.tlNavigationView, binding.mainView) { tab, position ->
+            Log.e("YMC", "ViewPager position: $position")
+//            tab.icon = iconList[position]
+            when (position) {
+                0 -> tab.text = "검색 결과"
+                1 -> tab.text = "내 보관함"
+                2 -> tab.text = "test"
+
+            }
+        }.attach()
+    }
+}
