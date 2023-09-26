a5b6e27101ca8b2e2ccdf584d1086742941bd46d
우재훈 <wooja6366@naver.com>
2023-09-26 09:53:29 +0900

    [FEAT] : ⚙️ HomePageUI,HomePageFragmentUI 설정

---
+++ app/src/main/java/com/example/bluecodingtube/bestRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.bluecodingtube
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class bestRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_best_recycler_view)
+    }
+}
