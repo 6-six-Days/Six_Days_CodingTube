a31f2cbac400583ac59759dd97a04797aac32557
youneeo <steamdacut@gmail.com>
2023-09-26 21:03:55 +0900

    [NEW] : 🆕 Splash 추가

---
+++ app/src/main/java/com/example/bluecodingtube/Splash.kt
@@ -0,0 +1,24 @@
+package com.example.bluecodingtube
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+            overridePendingTransition(R.anim.slide_down_enter,R.anim.slide_down_exit)
+            finish()
+        }, 1500)
+    }
+}
