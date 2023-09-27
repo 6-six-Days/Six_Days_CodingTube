81b327d8e683876f37ff17746dffb31650790d53
iWantBigMan <skarndgus8899@gmail.com>
2023-09-27 13:53:05 +0900

    [FEAT] : ⚙️ API KEY

---
+++ app/src/main/java/com/example/bluecodingtube/service/api/SixDays.kt
@@ -0,0 +1,23 @@
+package com.example.bluecodingtube.service.api
+
+import android.app.Application
+import com.example.bluecodingtube.service.api.SixDays.Companion.app
+
+class SixDays  : Application() {
+
+    companion object {
+        // 메인 메모리에만 저장되며, 멀티 쓰레드 환경에서 메인 메모리의 값을 참조하므로 변수 값 불일치 문제를 해결함
+        @Volatile
+        private lateinit var app: SixDays
+
+        @JvmStatic
+        fun getApp(): SixDays {
+            return app
+        }
+    }
+
+    override fun onCreate() {
+        app = this
+        super.onCreate()
+    }
+}
