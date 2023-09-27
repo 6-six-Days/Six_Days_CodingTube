70f68d15c6e04ccae03edef543674eca61126722
iWantBigMan <skarndgus8899@gmail.com>
2023-09-27 13:51:25 +0900

    [FEAT] : ⚙️ 레트로핏 구현 중

---
+++ app/src/main/java/com/example/bluecodingtube/service/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package com.example.bluecodingtube.service
+
+import okhttp3.OkHttpClient
+import okhttp3.internal.platform.Platform
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(AuthorizationInterceptor())
+            .build()
+    }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+
+            .build()
+    }
+    val search : YouTubeService by lazy {
+        retrofit.create(YouTubeService::class.java)
+    }
+
+
+
+}
