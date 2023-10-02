ba25156caa53e63ec83ed5c231b7ec647e5a153e
hwajiny <ghkwls02224@gmail.com>
2023-10-02 14:34:32 +0900

    [FEAT] : ⚙️ MyPage 리사이클러뷰/어뎁터 추가

--- app/src/main/java/com/example/bluecodingtube/MainActivity.kt
+++ app/src/main/java/com/example/bluecodingtube/MainActivity.kt
@@ -7,11 +7,13 @@ import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.bluecodingtube.databinding.ActivityMainBinding
+import com.example.bluecodingtube.viewModel.SearchItemModel
 import com.example.codingtube.adapter.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var likedItems: ArrayList<SearchItemModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
